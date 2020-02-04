7cac84ac740f432c904ca7c92549919b1cfa0730
Andrew Wilkinson <awilkinson@smartcommunications.com>
2020-02-04 13:30:07 +0000

    Remove wildcard import

--- src/test/java/abwilkinson/discordspringstarter/config/JdaSetupTests.java
+++ src/test/java/abwilkinson/discordspringstarter/config/JdaSetupTests.java
@@ -12,7 +12,11 @@ import org.springframework.web.client.RestTemplate;
 import javax.security.auth.login.LoginException;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class JdaSetupTests {
     private final JdaProperties properties = new JdaProperties();
